a6c257158525ba693a179cd5b994977cfd3bd420
Tej Charfi <charfi@usc.edu>
2020-08-16 21:38:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -99,15 +99,12 @@ def adding_matz
     }
     programmer_hash[:yukihiro_matsumoto] ={ 
       
-     {:known_for = "Ruby",
-     :languages = ["LISP","C"]}
+     :known_for => "Ruby",
+     :languages => ["LISP","C"]
       
     }
     
-    programmer_hash[:yukihiro_matsumoto] => {
-        :known_for => "Ruby",   
-        :languages => ["LISP", "C"]
-    }
+    
     programmer_hash
 end
 
