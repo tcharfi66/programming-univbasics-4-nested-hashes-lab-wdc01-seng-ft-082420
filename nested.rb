cb7d1250f6c3337cd5d4c12664af3ca35dc59c08
Tej Charfi <charfi@usc.edu>
2020-08-16 21:41:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -98,7 +98,7 @@ def adding_matz
       }
     }
     programmer_hash[:yukihiro_matsumoto] ={ 
-      
+    
      :known_for => "Ruby",
      :languages => ["LISP","C"]
       
@@ -127,7 +127,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+  
+  programmer_hash[:alan_kay][:known_for]= "GUI"
+  programmer_hash
 end
 
 def adding_to_dennis
