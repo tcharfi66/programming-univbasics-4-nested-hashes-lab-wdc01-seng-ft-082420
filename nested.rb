3d6aeda90d1645d440462c013a409f59692238c4
Tej Charfi <charfi@usc.edu>
2020-08-16 21:42:21 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require "pry"
 def hopper
 	programmer_hash = 
  		{
@@ -16,7 +16,15 @@ def hopper
       }
     }
 
+programmer_hash[:grace_hopper]
+
+
 end
+
+
+
+
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -36,8 +44,13 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for]
 
 end
+
+
+
+
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -56,6 +69,9 @@ def dennis_ritchies_language
       }
     }
 
+
+
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -81,7 +97,15 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:yukihiro_matsumoto] ={ 
+    
+     :known_for => "Ruby",
+     :languages => ["LISP","C"]
+      
+    }
+    
+    
+    programmer_hash
 end
 
 def changing_alan
@@ -103,7 +127,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+  
+  programmer_hash[:alan_kay][:known_for]= "GUI"
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +151,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+  
+  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+  programmer_hash
 end
